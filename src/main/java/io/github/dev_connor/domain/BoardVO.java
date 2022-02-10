99d8441a97004c7cf67089988162f5b27e0ebd7d
Connor <kor.connor@gmail.com>
2022-02-10 17:06:55 +0900

    DB 연결 후 테스트 완료

---
+++ src/main/java/io/github/dev_connor/domain/BoardVO.java
@@ -0,0 +1,18 @@
+package io.github.dev_connor.domain;
+
+import java.util.Date;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+
+@Data
+@Alias("BoardVO")
+public class BoardVO {
+	private long bno;
+	private String title; 
+	private String content;
+	private String writer;
+	private Date regdate; 
+	private Date updateDate;
+}
---
+++ src/main/java/io/github/dev_connor/mapper/BoardMapper.java
@@ -0,0 +1,19 @@
+package io.github.dev_connor.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import io.github.dev_connor.domain.BoardVO;
+import io.github.dev_connor.domain.Criteria;
+
+public interface BoardMapper {
+	public List<BoardVO> getList();
+	public void insert(BoardVO board);
+	public void insertSelectKey(BoardVO board);
+	public BoardVO read(Long bno); // p 193
+	public int delete(Long bno);   // p 194
+	public int update(BoardVO board); // p 196
+	public List<BoardVO> getListWithPaging(Criteria cri); // p 294
+//	public int getTotalCount(Criteria cri); // p 322
+}
